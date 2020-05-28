b8f62639f4efc3bd1d5e68acddbae9fb2d8f9c67
Diya Mahendru <36438396+diyamahendru@users.noreply.github.com>
2020-05-28 16:00:57 +0530

    epicc

--- Sorting and Searching/winningScholarship.cpp
+++ Sorting and Searching/winningScholarship.cpp
@@ -1,24 +1,28 @@
 #include<iostream>
+#define lli long long int
 
 using namespace std;
 
-bool possible(int n, int m, int x, int y, int cmax){
-    return cmax*x <= m + (n-cmax)*y;
+bool possible(lli n, lli m, lli x, lli y, lli cmax){
+    if(cmax*x <= m + (n-cmax)*y)
+        return true;
+    else 
+        return false;
 }
 
 int main(){
 
-    int n, m, x, y;
+    lli n, m, x, y;
     cin>>n>>m>>x>>y;
 
     //binary search to maximise the number of students who get the scholarship
-    int f = 0;
-    int l = n;
-    int ans = 0;
+    lli f = 0;
+    lli l = n;
+    lli ans = 0;
     while(f<=l){
-        int mid = (f+l)/2;
+        lli mid = (f+l)/2;
         if(possible(n,m,x,y,mid)){
-            ans = mid;
+            ans = max(mid, ans);
             f = mid+1;
         }
         else
