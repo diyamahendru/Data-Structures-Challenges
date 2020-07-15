114e93f924641e2c33f5e47038bf436281a062de
Diya Mahendru <36438396+diyamahendru@users.noreply.github.com>
2020-07-15 22:38:34 +0530

    Better

---
+++ Number Theory/greatestCommonDivisor.cpp
@@ -0,0 +1,23 @@
+// The challenge is to find the GCD of two numbers provided using recursion.
+
+#include<iostream>
+
+using namespace std;
+
+int gcd(int x, int y){
+    if(y==0)
+        return x;
+    else
+        return gcd(y, x%y);
+}
+
+int main(){
+    int a, b;
+    cin>>a>>b;
+
+    int res = gcd(a, b);
+
+    cout<<res<<endl;
+    
+    return 0;
+}
