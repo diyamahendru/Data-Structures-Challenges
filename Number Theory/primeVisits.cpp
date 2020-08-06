ea586e28fffc71fc9dbca96aa64fadba06fe8d19
Diya Mahendru <36438396+diyamahendru@users.noreply.github.com>
2020-08-06 14:07:46 +0530

    Updates

--- Number Theory/primeVisits.cpp
+++ Number Theory/primeVisits.cpp
@@ -1,1 +1,47 @@
-//The challenge is to find the prime numbers between two given numbers
+//The challenge is to find the prime numbers between two given numbers.
+
+#include<iostream>
+#define ll long long 
+
+using namespace std;
+
+void seive(int *p, int n){
+    for(int i=3; i<=n; i+=2){
+        p[i] = 1;
+    }
+
+    for(ll i=3; i<=n; i+=2){
+        if(p[i]==1){
+            for(ll j=i*i; j<n; j+=i){
+                p[j] = 0;
+            }
+        }
+    }
+
+    p[2] = 1;
+    p[1] = p[0] = 0;
+}
+
+int main(){
+
+    ll n = 10000;
+
+    int primes[n] = {0};
+    seive(primes, n);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int a, b;
+        cin>>a>>b;
+        int c=0;
+        for(int i=a; i<=b; i++){
+            if(primes[i] == 1){
+                c++;
+            }
+        }
+        cout<<c<<endl;
+    }
+
+    return 0;
+}
---
+++ Recursions/stringsWithNoConsec1s.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int count(int n){
+    if(n==0)
+        return 1;
+    if(n==1)
+        return 2;
+    if(n<0)
+        return 0;
+    int cnt = 0;
+    cnt = count(n-1) + count(n-2);
+
+    return cnt;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    cout<<count(n);
+    return 0;
+}
