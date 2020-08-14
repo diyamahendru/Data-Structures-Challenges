b65ecd3426533059d9ac52670e8da73676eca98f
Diya Mahendru <36438396+diyamahendru@users.noreply.github.com>
2020-08-15 00:31:17 +0530

    Remaining

---
+++ Recursions/dictionarySmall.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<set>
+
+using namespace std;
+
+void generate(char *in, int i, set<string> &s){
+    if(in[i] == '\0'){
+        string t(in);
+        s.insert(t);
+        return;
+    }
+    for(int j=i; in[j] != '\0'; j++){
+        //Going down
+        swap(in[i], in[j]);
+        //Recursion
+        generate(in, i+1, s);
+        //Back-track
+        swap(in[i], in[j]);
+    }
+}
+
+int main(){
+    char in[25];
+    cin>>in;
+
+    set<string> s;
+    generate(in, 0, s);
+
+    for(auto str:s){
+        if(str<in)
+            cout<<str<<endl;
+    }
+
+    return 0;
+}
